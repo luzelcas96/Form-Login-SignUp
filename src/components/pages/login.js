138eb911d99634929446e8cbfaf5064b70c8b15d
Lucho Zelada <luisdanger96@gmail.com>
2022-06-22 02:07:19 -0500

    CHORE:  clean component

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,21 +1,6 @@
-import styled from "@emotion/styled";
 import Button from "../Button";
 import FormInput from "../FormInput";
-import { colors, typography } from "../../styles";
-import { TitleForm, Wrapper, WrapperForm } from "./styles";
-
-const newButton = {
-    width:"100%", 
-    backgroundColor: colors.primary[300],
-    color: colors.white,
-    fontFamily: "Inter",
-    fontStyle: "normal",
-    fontWeight: "500",
-    fontSize: "14px",
-    lineHeight: "20px",
-    letterSpacing:"0.1em",
-    textTransform: "Uppercase"
-}
+import { newButton, TitleForm, Wrapper, WrapperForm } from "./styles";
 
 function Login() {
     return (
